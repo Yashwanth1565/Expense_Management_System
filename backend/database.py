2075ee91cdc455e2013b1c2ce96718b108dedadf
Yashwanth Boya <yashwanthboya27@gmail.com>
2026-04-03 14:43:32 +0530

    Change database from PostgreSQL to SQLite

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:yash1565@localhost:5432/expense_db"
+DATABASE_URL = "sqlite:///./expenses.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
